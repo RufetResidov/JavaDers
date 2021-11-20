c60e28f1fbcb7d732322c601cedf0da0151052b3
RufetResidov <79075563+RufetResidov@users.noreply.github.com>
2021-11-20 15:46:13 +0400

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,22 @@
+public class main {
+    public static void main(String[] args) {
+        String[] a = {"Illegal", "Bot", "Java", "Təqdimat", "God", "Start"};
+        System.out.println(a.length);
+        int count =0;
+        String[] b = {"a", "i", "ı", "o", "u", "e","ü","ö"};
+
+        for(int i =0 ; i < a.length; i++) {
+            if (charAt(i) == a ) {
+                count++;
+            }
+            else{
+
+            }
+        }
+
+    }
+
+}
+
+
+
